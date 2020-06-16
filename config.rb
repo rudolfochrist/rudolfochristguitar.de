41dd1304591d1cd25e716cfe544ba6770e80555f
Sebastian Christ <rudolfo.christ@gmail.com>
2020-06-17 00:12:23 +0200

    Initial commit

---
+++ config.rb
@@ -0,0 +1,58 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+activate :directory_indexes
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+helpers do
+  def year_range
+    year_started = 2020
+    current_year = Date.today.year
+    return year_started if year_started == current_year
+    return "#{year_started} - #{current_year}"
+  end
+
+  def active_nav(path)
+    if path == "/" and current_page.url == "/"
+      return "active"
+    end
+    return "active" if current_page.url.start_with? path
+  end
+end
+
+# build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
