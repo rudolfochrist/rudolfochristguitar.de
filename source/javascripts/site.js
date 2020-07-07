f69b6e9d0ebdc8581dd838287e041cf65aa42844
Sebastian Christ <rudolfo.christ@gmail.com>
2020-07-07 11:20:36 +0200

    Add agenda page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,22 @@
 // This is where it all goes :)
+
+function docReady(fn) {
+  if (document.readyState !== "loading") {
+    fn();
+  }
+  else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+function showNoUpcomingEventsNote() {
+  const upcomingEvents = document.getElementById("upcoming-events");
+  const upcomingNote = document.getElementById("no-upcoming-note");
+  if (upcomingEvents && upcomingEvents.children.length === 0) {
+    upcomingNote.classList.remove('hidden');
+  }
+}
+
+docReady(() => {
+  showNoUpcomingEventsNote();
+});
