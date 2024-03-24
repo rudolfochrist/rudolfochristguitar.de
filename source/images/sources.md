f7278ccdc5ba25aaa04f724705f25b61e9620db1
Sebastian Christ <rudolfo.christ@gmail.com>
2024-03-24 22:16:50 +0100

    Implement mobile nav

---
+++ source/images/sources.md
@@ -0,0 +1,5 @@
+
+# Icons
+
+- Instagram:[https://www.iconfinder.com/icons/5305164/play_video_youtube_youtube_logo_icon](https://www.iconfinder.com/icons/5335781/camera_instagram_social_media_instagram_logo_icon)
+- YouTube: [https://www.iconfinder.com/icons/5305164/play_video_youtube_youtube_logo_icon](https://www.iconfinder.com/icons/5305164/play_video_youtube_youtube_logo_icon)
--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,5 +1,20 @@
 // This is where it all goes :)
 
+const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+const anchorLinks = document.querySelectorAll(".anchor-link");
+const iconClose = document.querySelector(".icon-close");
+const iconBurger = document.querySelector(".icon-burger");
+
+mobileNavToggle.addEventListener("click", toggleNav);
+anchorLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleNav();
+    window.location = e.target.href;
+  });
+});
+
 function docReady(fn) {
   if (document.readyState !== "loading") {
     fn();
@@ -35,9 +50,11 @@ function sortEvents() {
   }
 }
 
-docReady(() => {
-  const events = document.querySelector("#events");
-  if (events != null) {
-    sortEvents();
-  }
-});
+function toggleNav() {
+  primaryNav.toggleAttribute("data-visible");
+  iconBurger.classList.toggle("display-none");
+  iconClose.classList.toggle("display-none");
+  primaryNav.hasAttribute("data-visible")
+    ? mobileNavToggle.setAttribute("aria-expanded", true)
+    : mobileNavToggle.setAttribute("aria-expanded", false);
+}
